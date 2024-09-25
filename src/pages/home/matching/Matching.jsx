9dbd43e89845b7b8d571452ef58c2be92508db29
MizukiKusumoto <apple1167water@gmail.com>
2024-09-25 16:07:07 +0900

    おすすめユーザーの表示

--- src/pages/home/matching/Matching.jsx
+++ src/pages/home/matching/Matching.jsx
@@ -1,24 +1,63 @@
-import React from 'react';
-import { Stack } from '@mui/material';
+import React, { useContext, useState, useEffect } from 'react';
+import { Box, Button, Stack } from '@mui/material';
 import MatchedUser from './components/matchedUser/MatchedUser';
 import Share from './components/share/Share';
+import axios from 'axios';
+import { AuthContext } from '../../../state/AuthContext';
 
 export default function Matching() {
+  const { user } = useContext(AuthContext);
+  const [matchedUsers, setMatchedUsers] = useState([]);
+  const [count, setCount] = useState(2);
+
+  const handleMore = () => {
+    setCount(count + 2);
+  };
+
+  useEffect(() => {
+    const fetchMatchedUsers = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/user/similar/${user.id}`
+        );
+        setMatchedUsers(response.data || []);
+      } catch (error) {
+        console.error('Error fetching matched users', error);
+      }
+    };
+
+    fetchMatchedUsers();
+  }, [user]);
+
   return (
     <Stack flex={3} spacing={5} sx={{ padding: '20px 100px' }}>
-      <Stack spacing={1}>
-        <h2>おすすめのユーザー：</h2>
-        <MatchedUser
-          userName={'Mizuki'}
-          matchedTags={['a', 'b']}
-          percentage={99}
-        />
-        <MatchedUser
-          userName={'Mizu'}
-          matchedTags={['ccccccccccccccccccccccccccc']}
-          percentage={10}
-        />
-      </Stack>
+      <Box>
+        <Stack spacing={1}>
+          <h2>おすすめのユーザー：</h2>
+          {matchedUsers.slice(0, count).map((matchedUser, index) => (
+            <MatchedUser
+              key={index}
+              userName={matchedUser.name}
+              matchedTags={matchedUser.labels ? matchedUser.labels : []}
+              percentage={(matchedUser.similarity * 100).toFixed(2)} // 小数点を2桁にする
+            />
+          ))}
+        </Stack>
+        {count < matchedUsers.length && (
+          <Button
+            onClick={handleMore}
+            color='primary'
+            sx={{
+              alignSelf: 'flex-start',
+              marginTop: '10px',
+              textDecoration: 'underline',
+            }}
+          >
+            さらに読み込む
+          </Button>
+        )}
+      </Box>
+
       <Share />
     </Stack>
   );
--- src/pages/home/matching/components/matchedUser/MatchedUser.jsx
+++ src/pages/home/matching/components/matchedUser/MatchedUser.jsx
@@ -5,7 +5,7 @@ import CardContent from '@mui/material/CardContent';
 import Avatar from '@mui/material/Avatar';
 import Typography from '@mui/material/Typography';
 import { red } from '@mui/material/colors';
-import { Chip, Grid2, LinearProgress, Stack } from '@mui/material';
+import { Chip, Stack, LinearProgress, Box } from '@mui/material';
 
 export default function MatchedUser({
   icon_url,
@@ -22,20 +22,15 @@ export default function MatchedUser({
           </Avatar>
         }
         title={userName}
+        titleTypographyProps={{
+          fontSize: '30px',
+        }}
         sx={{ padding: '10px 10px 0 10px' }}
       />
       <CardContent sx={{ padding: '0px 10px 0 65px' }}>
-        <Grid2 container>
-          <Grid2 size={3}>
-            <Typography
-              variant='body2'
-              sx={{ color: 'text.secondary' }}
-              flex={2}
-            >
-              あなたとマッチした属性：
-            </Typography>
-          </Grid2>
-          <Grid2 size={9}>
+        <Stack spacing={2}>
+          <Stack direction='row' alignItems='center' spacing={1}>
+            <Typography>あなたとマッチした属性：</Typography>
             <Stack direction='row' flexWrap='wrap'>
               {matchedTags.map((tag) => (
                 <Chip
@@ -43,36 +38,43 @@ export default function MatchedUser({
                   label={tag}
                   color='primary'
                   sx={{
-                    margin: '0 5px 5px 5px',
+                    margin: '0 5px 5px 0',
                     minWidth: '60px',
                     backgroundColor: 'purple',
                   }}
                 />
               ))}
             </Stack>
-          </Grid2>
-        </Grid2>
-        <Grid2 container>
-          <Grid2 size={2}>
-            <Typography variant='body2' sx={{ color: 'text.secondary' }}>
-              マッチ度：{percentage}%
-            </Typography>
-          </Grid2>
-          <Grid2 size={10}>
-            <LinearProgress
-              variant='determinate'
-              value={percentage}
-              color='primary'
-              sx={{
-                height: '15px',
-                marginTop: '3px',
-                '&.MuiLinearProgress-root': {
-                  backgroundColor: 'transparent',
-                },
-              }}
-            />
-          </Grid2>
-        </Grid2>
+          </Stack>
+
+          <Stack direction='row' alignItems='center' spacing={2}>
+            <Typography>マッチ度：</Typography>
+            <Box sx={{ position: 'relative', flexGrow: 1 }}>
+              <LinearProgress
+                variant='determinate'
+                value={percentage}
+                color='primary'
+                sx={{
+                  height: '15px',
+                  borderRadius: '5px',
+                }}
+              />
+              <Typography
+                variant='body2'
+                sx={{
+                  position: 'absolute',
+                  top: 0,
+                  left: '5px',
+                  color: 'white',
+                  fontWeight: 'bold',
+                  lineHeight: '15px',
+                }}
+              >
+                {percentage}%
+              </Typography>
+            </Box>
+          </Stack>
+        </Stack>
       </CardContent>
     </Card>
   );
